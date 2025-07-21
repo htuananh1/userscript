0dfde875ecb713ce792a14e22bef466bf58846bf
htuananh1 <83439368+htuananh1@users.noreply.github.com>
2025-07-21 22:39:03 +0700

    Update and rename manifest.json to Yeumoney.js

---
+++ Yeumoney.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Hades Auto - Bypass Yeumoney
+// @namespace    http://tampermonkey.net/
+// @version      1.4
+// @description  Đợi đủ 80s mới lấy mã, tự động nhập, submit cho Yeumoney, và fake trình duyệt khi gọi API!
+// @author       htuananh + ChatGPT
+// @match        https://yeumoney.com/*
+// @connect      traffic-user.net
+// @icon         https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGhmdmJyN3cxdWNjNDc1aG5iN3J4eTBrMWV6Z3lscTh0MHFnemV0diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/jPdNzfqIDmokLbSqO0/giphy.gif
+// @grant        GM.xmlHttpRequest
+// @grant        GM_setClipboard
+// @grant        GM_notification
+// @grant        GM_addStyle
+// @run-at       document-end
+// ==/UserScript==
