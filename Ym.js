89bee4329f7c2ee62398b99b6c5ba1da8620449b
htuananh1 <83439368+htuananh1@users.noreply.github.com>
2025-07-22 19:51:55 +0700

    Update Ym.js

--- Ym.js
+++ Ym.js
@@ -23,7 +23,7 @@
     // --- Cấu hình các site ---
     const sites = {
         'm88':      { codexn: 'taodeptrai', url: 'https://bet88ec.com/cach-danh-bai-sam-loc', loai_traffic: 'https://bet88ec.com/', span_id: 'layma_me_vuatraffic', api_file: 'GET_MA.php', param: 'codexn' },
-        'fb88':     { codexn: 'taodeptrai', url: 'https://fb88mg.com/ty-le-cuoc-hong-kong-la-gi', loai_traffic: 'https://fb88mg.com/', span_id: 'layma_me_vuatraffic', api_file: 'GET_MA.php', param: 'codexn' },
+        'fb88':     { codexn: 'taodeptrai', url: 'https://fb88dq.com/ty-le-cuoc-hong-kong-la-gi', loai_traffic: 'https://fb88dq.com/', span_id: 'layma_me_vuatraffic', api_file: 'GET_MA.php', param: 'codexn' },
         '188bet':   { codexn: 'taodeptrailamnhe', url: 'https://88betag.com/cach-choi-game-bai-pok-deng', loai_traffic: 'https://88betag.com/', span_id: 'layma_me_vuatraffic', api_file: 'GET_MA.php', param: 'codexn' },
         'w88':      { codexn: 'taodeptrai', url: 'https://188.166.185.213/tim-hieu-khai-niem-3-bet-trong-poker-la-gi', loai_traffic: 'https://188.166.185.213/', span_id: 'layma_me_vuatraffic', api_file: 'GET_MA.php', param: 'codexn' },
         'v9bet':    { codexn: 'taodeptrai', url: 'https://v9betho.com/ca-cuoc-bong-ro-ao', loai_traffic: 'https://v9betho.com/', span_id: 'layma_me_vuatraffic', api_file: 'GET_MA.php', param: 'codexn' },
