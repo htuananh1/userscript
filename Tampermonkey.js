56795467e363db1e3ee61fa2ad84e7f892cd19b9
htuananh1 <83439368+htuananh1@users.noreply.github.com>
2025-07-23 14:31:44 +0700

    Update Tampermonkey.js

--- Tampermonkey.js
+++ Tampermonkey.js
@@ -5,7 +5,6 @@
 // @description  Tuấn Anh đẹp trai
 // @author       htuananh + ChatGPT
 // @match        https://yeumoney.com/*
-// @match        https:// linkday.xyz/*
 // @connect      traffic-user.net
 // @icon         https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGhmdmJyN3cxdWNjNDc1aG5iN3J4eTBrMWV6Z3lscTh0MHFnemV0diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/jPdNzfqIDmokLbSqO0/giphy.gif
 // @grant        GM.xmlHttpRequest
@@ -14,4 +13,5 @@
 // @grant        GM_addStyle
 // @run-at       document-start
 // @require      https://raw.githubusercontent.com/htuananh1/userscript/refs/heads/main/Ym.js
+// @updateURL    https://raw.githubusercontent.com/htuananh1/userscript/refs/heads/main/Ym.js
 // ==/UserScript==
